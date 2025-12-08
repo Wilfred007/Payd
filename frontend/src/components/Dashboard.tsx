5974ea79624b7245d3269f294f7e1e22209e2e2b
Wilfred007 <adzerwilfred007@gmail.com>
2025-12-08 02:01:37 +0100

    dashboard impl

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import BalanceCard from './BalanceCard'
+import DepositForm from './DepositForm'
+import RunPayroll from './RunPayroll'
+import Employees from './Employees'
+import History from './History'
+
+export default function Dashboard() {
+  return (
+    <div className="space-y-6">
+      <div className="grid-cards">
+        <BalanceCard />
+        <DepositForm />
+        <RunPayroll />
+      </div>
+      <Employees />
+      <History />
+    </div>
+  )
+}
